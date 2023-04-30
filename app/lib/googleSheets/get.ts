670d2b4859c02b54a68af596744b7032f9eafeb8
caolan <caolan@digigrow.ie>
2023-04-30 13:54:26 -0400

    adding in two fetch calls, one from header one for data

---
+++ app/lib/googleSheets/get.ts
@@ -0,0 +1,26 @@
+export async function getHeaderData() {
+  let res = await fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/1Y_yiT-_7IimioBvcqiCPwLzTLazfdRyzZ4k3cpQXiAw/values/A1:G1?key=AIzaSyC085kBESY6TaCyBt1RuhhjEFz1j0E33iM&majorDimension=COLUMNS`
+  );
+
+  if (!res.ok) throw "There was an error fetching data.";
+
+  let data = await res?.json();
+
+  console.log(data);
+
+  return data;
+}
+export async function getTableData() {
+  let res = await fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/1Y_yiT-_7IimioBvcqiCPwLzTLazfdRyzZ4k3cpQXiAw/values/A2:G2?key=AIzaSyC085kBESY6TaCyBt1RuhhjEFz1j0E33iM&majorDimension=ROWS`
+  );
+
+  if (!res.ok) throw "There was an error fetching data.";
+
+  let data = await res?.json();
+
+  console.log(data);
+
+  return data;
+}
