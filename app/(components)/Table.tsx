79d5bafbb5b98fcb4941c05404320c0645fa344b
caolan <caolan@digigrow.ie>
2023-04-30 13:53:27 -0400

    addding in promise.all to avoid waterfall loading

--- app/(components)/Table.tsx
+++ app/(components)/Table.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { getHeaderData, getTableData } from "../lib/googleSheets/get";
 
 type Props = {
   className?: string;
 };
 
-const Table = ({ className }: Props) => {
+const Table = async ({ className }: Props) => {
+  // const titleArray = values?.splice(0, 1);
+  const headerData = getHeaderData();
+  const tableData = getTableData();
+
+  const [header, table] = await Promise.all([headerData, tableData]);
+
   return (
     <section
       className={`flex flex-wrap basis-full content-start lg:basis-1/2 lg:flex-shrink justify-start overflow-hidden hover:overflow-x-scroll rounded-md ${
@@ -14,47 +21,29 @@ const Table = ({ className }: Props) => {
       <table className="shadow-md w-full">
         <thead className="">
           <tr className="">
-            <th className="p-3 border border-gray-50 bg-orange-300 text-white">
-              Asset Name
-            </th>
-            <th className="p-3 border border-gray-50 bg-orange-300 text-white">
-              Location
-            </th>
-            <th className="p-3 border border-gray-50 bg-orange-300 text-white">
-              Business Cateogory
-            </th>
-            <th className="p-3 border border-gray-50 bg-orange-300 text-white">
-              Risk Rating
-            </th>
-            <th className="p-3 border border-gray-50 bg-orange-300 text-white">
-              Risk Factors
-            </th>
-            <th className="p-3 border border-gray-50 bg-orange-300 text-white">
-              Year
-            </th>
+            {header.values?.map((value: string, index: number) => (
+              <th
+                key={index + 1}
+                className="p-3 border border-gray-50 bg-orange-300 text-white"
+              >
+                {value}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody className="">
-          <tr className="">
-            <td className="p-3 text-center text-slate-800 border bg-gray-100 border-gray-200 ">
-              McKnight
-            </td>
-            <td className="p-3 text-center text-slate-800 border bg-gray-100 border-gray-200 ">
-              Lat: 46, Long: 60
-            </td>
-            <td className="p-3 text-center text-slate-800 border bg-gray-100 border-gray-200 ">
-              Energy
-            </td>
-            <td className="p-3 text-center text-slate-800 border bg-gray-100 border-gray-200 ">
-              0.06
-            </td>
-            <td className="p-3 text-center text-slate-800 border bg-gray-100 border-gray-200 ">
-              Earthquake
-            </td>
-            <td className="p-3 text-center text-slate-800 border bg-gray-100 border-gray-200 ">
-              1950
-            </td>
-          </tr>
+          {table?.values?.map((value: Array<string>, index: number) => (
+            <tr key={index + 1} className="">
+              {value.map((innerValue: string, index: number) => (
+                <td
+                  key={index + 1}
+                  className="p-3 text-center text-slate-800 border bg-gray-100 border-gray-200"
+                >
+                  {innerValue}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </section>
