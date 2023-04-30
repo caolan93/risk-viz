b49c88bd264742f55bbddd0377d13b7ef2d27b1b
caolan <caolan@digigrow.ie>
2023-04-30 18:05:57 -0400

    google sheets init

--- app/lib/googleSheets/get.ts
+++ app/lib/googleSheets/get.ts
@@ -1,6 +1,6 @@
 export async function getHeaderData() {
   let res = await fetch(
-    `https://sheets.googleapis.com/v4/spreadsheets/1Y_yiT-_7IimioBvcqiCPwLzTLazfdRyzZ4k3cpQXiAw/values/A1:G1?key=AIzaSyC085kBESY6TaCyBt1RuhhjEFz1j0E33iM&majorDimension=COLUMNS`
+    `https://sheets.googleapis.com/v4/spreadsheets/1Y_yiT-_7IimioBvcqiCPwLzTLazfdRyzZ4k3cpQXiAw/values/A1:G1?key=AIzaSyC085kBESY6TaCyBt1RuhhjEFz1j0E33iM`
   );
 
   if (!res.ok) throw "There was an error fetching data.";
@@ -13,7 +13,7 @@ export async function getHeaderData() {
 }
 export async function getTableData() {
   let res = await fetch(
-    `https://sheets.googleapis.com/v4/spreadsheets/1Y_yiT-_7IimioBvcqiCPwLzTLazfdRyzZ4k3cpQXiAw/values/A2:G2?key=AIzaSyC085kBESY6TaCyBt1RuhhjEFz1j0E33iM&majorDimension=ROWS`
+    `https://sheets.googleapis.com/v4/spreadsheets/1Y_yiT-_7IimioBvcqiCPwLzTLazfdRyzZ4k3cpQXiAw/values/A2:G2?key=AIzaSyC085kBESY6TaCyBt1RuhhjEFz1j0E33iM`
   );
 
   if (!res.ok) throw "There was an error fetching data.";
