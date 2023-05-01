421381585ba6121a0b7e6d610e46bf883ab897f0
caolan <caolan@digigrow.ie>
2023-05-01 13:02:52 -0400

    table update

--- app/(components)/table/TableBody.tsx
+++ app/(components)/table/TableBody.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  table: TableData[];
+  cols: {
+    label: string;
+    key: string;
+  }[];
+};
 
-const TableBody = (props: Props) => {
+const TableBody = ({ table, cols }: Props) => {
   return (
     <tbody className="bg-orange-50">
-      <tr>
-        <td className="font-bold text-center border-r border-gray-300 p-3">
-          Data Row
-        </td>
-        <td className="font-bold text-center border-r border-gray-300 p-3">
-          Data Row
-        </td>
-        <td className="font-bold text-center border-r border-gray-300 p-3">
-          Data Row
-        </td>
-        <td className="font-bold text-center border-r border-gray-300 p-3">
-          Data Row
-        </td>
-        <td className="font-bold text-center p-3">Data Row</td>
-      </tr>
+      {table?.map((value, index: number) => (
+        <tr key={index + 1} className="even:bg-red-100">
+          {value.map((innerValue, index: number) => {
+            return (
+              <td
+                key={cols[index].key}
+                className="font-bold text-center border-r border-gray-300 p-3 "
+              >
+                {innerValue}
+              </td>
+            );
+          })}
+        </tr>
+      ))}
     </tbody>
   );
 };
--- app/(components)/table/TableHeader.tsx
+++ app/(components)/table/TableHeader.tsx
@@ -1,16 +1,30 @@
+"use client";
+
 import React from "react";
 
-type Props = {};
+type Props = {
+  header?: DataType;
+  cols: {
+    label: string;
+    key: string;
+  }[];
+};
 
-const TableHeader = (props: Props) => {
+const TableHeader = ({ cols }: Props) => {
+  const handleAccessor = (key: string) => {
+    () => console.log("Acc", key);
+  };
   return (
-    <thead className="bg-gray-200 text-gray-600">
+    <thead className="bg-red-200 text-gray-200">
       <tr className="border-b border-gray-300">
-        <th className="border-r border-gray-300 p-3">Data Header</th>
-        <th className="border-r border-gray-300 p-3">Data Header</th>
-        <th className="border-r border-gray-300 p-3">Data Header</th>
-        <th className="border-r border-gray-300 p-3">Data Header</th>
-        <th className="p-3">Data Header</th>
+        {cols?.map((col) => (
+          <th
+            key={col.key}
+            className="cursor-pointer border-r border-gray-300 p-3 text-black"
+          >
+            {col.label}
+          </th>
+        ))}
       </tr>
     </thead>
   );
