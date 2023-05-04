1db79f69e15def0d8c0eb7449d92ebcf0a557672
caolan <caolan@digigrow.ie>
2023-05-04 18:54:22 -0400

    updating table to call table styling

--- app/(components)/table/Table.tsx
+++ app/(components)/table/Table.tsx
@@ -7,24 +7,36 @@ import { getHeaderData, getTableDataJSON } from "@/app/lib/googleSheets/get";
 import Spinner from "../Spinner";
 import { paginationNext, paginationPrev } from "@/app/lib/googleSheets/utils";
 
+// REDUX
+import { RootState } from "@/app/GlobalRedux/store";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  getData,
+  sortTable,
+} from "@/app/GlobalRedux/Features/googleRange/googleRange";
+
 type Props = {};
 
 const Table = (props: Props) => {
-  const [headerData, setHeaderData] = useState<DataType>();
-  const [tableData, setTableData] = useState<ObjectTable[]>([]);
-  const [range, setRange] = useState("");
-  const [pageVal, setPalVal] = useState(null);
+  const range = useSelector((state: RootState) => state.googleRange.value);
+  const tableData = useSelector(
+    (state: RootState) => state.googleRange.tableData
+  );
+  const dispatch = useDispatch();
+
+  const [isDisabled, setIsDisabled] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchData() {
-      const [tableData, range] = await getTableDataJSON();
-      setRange(range);
-      setTableData(tableData);
-      setIsLoading(false);
-    }
-    fetchData();
-  }, []);
+    dispatch(getData(range));
+    setIsLoading(false);
+
+    // if (range === "A2:G11") {
+    //   setIsDisabled(true);
+    // }
+  }, [range]);
 
   let cols = [
     { label: "Asset Name", key: "asset_name", sortable: true },
@@ -49,32 +61,16 @@ const Table = (props: Props) => {
           }) * (sortOrder === "asc" ? 1 : -1)
         );
       });
-      setTableData(sorted);
+      dispatch(sortTable(sorted));
     }
   };
 
   const nextCols = async (value: string) => {
-    let data = paginationNext(value);
-
-    console.log("next data", data);
-
-    setIsLoading(true);
-    const [tableData, range] = await getTableDataJSON(data);
-    setTableData(tableData);
-    setRange(range);
-    setIsLoading(false);
+    dispatch(increment());
   };
 
   const prevCols = async (value: string) => {
-    let data = paginationPrev(value);
-
-    console.log("next data", data);
-
-    setIsLoading(true);
-    const [tableData, range] = await getTableDataJSON(data);
-    setTableData(tableData);
-    setRange(range);
-    setIsLoading(false);
+    dispatch(decrement());
   };
 
   return (
@@ -84,12 +80,12 @@ const Table = (props: Props) => {
           <Spinner />
         </div>
       ) : (
-        <div className="flex flex-col overflow-hidden">
+        <div className="flex flex-col overflow-hidden w-full">
           <h2 className="text-xl text-center font-bold my-4">
             Displaying columns {range}
           </h2>
-          <div className="overflow-x-scroll">
-            <table className=" bg-gray-50 border border-gray-300 p-3 rounded-md shadow-lg shadow-gray-300  border-none ">
+          <div className="overflow-x-scroll md:overflow-hidden">
+            <table className=" bg-gray-50 border border-gray-300 p-3 rounded-md shadow-lg shadow-gray-300  border-none w-full md:h-[800px]">
               <TableHeader cols={cols} handleSorting={handleSorting} />
               <TableBody cols={cols} table={tableData} />
             </table>
@@ -97,12 +93,14 @@ const Table = (props: Props) => {
           <div className="my-4 mx-auto flex content-start overflow-hidden justify-center">
             <button
               onClick={() => prevCols(range)}
+              disabled={range === "A2:G11" && true}
               className="py-2 px-4 bg-blue-400 text-white font-bold rounded-md scale-95 hover:scale-100 ease duration-200 disabled:opacity-60 disabled:hover:scale-95"
             >
               Load Prev Data
             </button>
             <button
               onClick={() => nextCols(range)}
+              disabled={range === "A4990:G5001" && true}
               className="py-2 px-4 bg-blue-400 text-white font-bold rounded-md scale-95 hover:scale-100 ease duration-200 disabled:opacity-60 disabled:hover:scale-95"
             >
               Load Next Data
