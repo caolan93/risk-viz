cf0b0ee9ba3667dbacfa46d3d722316e1d0e2c1e
caolan <caolan@digigrow.ie>
2023-05-04 15:02:48 -0400

    deleting api route

--- app/api/data/route.ts
+++
@@ -1,8 +0,0 @@
-export async function getAllData(request: Request) {
-  try {
-    const response = fetch(`${process.env.GOOGLE_DOC_URL}`);
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
-}
