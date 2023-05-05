085df7ad3ab8d8df50734ff8178128aa29306e07
caolan <caolan@digigrow.ie>
2023-05-04 23:48:16 -0400

    making line chart responsive

--- app/(components)/chart/LineChart.tsx
+++ app/(components)/chart/LineChart.tsx
@@ -7,32 +7,71 @@ import { CategoryScale } from "chart.js";
 
 import data from "../data.json";
 
+// REDUX
+import { RootState } from "@/app/GlobalRedux/store";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  getData,
+  sortTable,
+} from "@/app/GlobalRedux/Features/googleRange/googleRange";
+
 Chart.register(CategoryScale);
 
 type Props = {
-  tableData: TableData;
+  tableData: ObjectTable;
 };
 
-const LineChart = ({ tableData }: Props) => {
+const LineChart = () => {
+  const range = useSelector((state: RootState) => state?.googleRange?.value);
+  const tableData = useSelector(
+    (state: RootState) => state?.googleRange?.tableData
+  );
+  const dispatch = useDispatch();
+
   const [chartData, setChartData] = useState({
-    labels: data.values?.map((val) => val[6]).sort(),
+    labels: tableData?.map((value: ObjectTable) => value?.year).sort(),
     datasets: [
       {
         label: "Risk Rating",
-        data: data.values?.map((val) => val[4]),
+        data: tableData?.map((value: ObjectTable) => value?.risk_rating),
         borderColor: "black",
         borderWidth: 2,
       },
     ],
   });
 
+  useEffect(() => {
+    dispatch(getData(range));
+  }, [range]);
+  useEffect(() => {
+    handleChartData(tableData);
+  }, [tableData]);
+
+  const handleChartData = (data: ObjectTable[]) => {
+    setChartData({
+      labels: data?.map((value: ObjectTable) => value?.year).sort(),
+      datasets: [
+        {
+          label: "Risk Rating",
+          data: data?.map((value: ObjectTable) => value?.risk_rating),
+          borderColor: "black",
+          borderWidth: 2,
+        },
+      ],
+    });
+  };
+
   return (
-    <div className="flex flex-col">
-      <h2 className="basis-full">Line Chart Graph</h2>
-      <div className="flex basis-full p-9">
+    <div className="flex flex-col w-full">
+      <h2 className="flex  justify-center basis-full">Line Chart Graph</h2>
+      <div className="flex basis-full p-0 md:p-9">
         <Line
+          className="w-full"
           data={chartData}
           options={{
+            responsive: true,
             plugins: {
               title: {
                 display: true,
@@ -44,6 +83,22 @@ const LineChart = ({ tableData }: Props) => {
             },
           }}
         />
+      </div>
+      <div className="flex justify-center">
+        <button
+          onClick={() => dispatch(decrement())}
+          disabled={range === "A2:G11" && true}
+          className="py-2 px-4 bg-blue-400 text-white font-bold rounded-md scale-95 hover:scale-100 ease duration-200 disabled:opacity-60 disabled:hover:scale-95"
+        >
+          Load Prev Data
+        </button>
+        <button
+          onClick={() => dispatch(increment())}
+          disabled={range === "A4990:G5001" && true}
+          className="py-2 px-4 bg-blue-400 text-white font-bold rounded-md scale-95 hover:scale-100 ease duration-200 disabled:opacity-60 disabled:hover:scale-95"
+        >
+          Load Next Data
+        </button>
       </div>
     </div>
   );
